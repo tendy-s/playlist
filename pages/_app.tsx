c03d1da23298241e5c8aabacc6b7802f074f1fd0
grimSkepsis <developer.mikehofer@gmail.com>
2022-04-09 13:26:59 -0700

    updates assets

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,16 @@
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import '@fontsource/raleway/400.css'
+import '@fontsource/open-sans/700.css'
 import { StoreProvider } from "easy-peasy";
 import PlayerLayout from "../components/playerLayout";
 import { store } from "../lib/store";
 import "reset-css";
 
 const theme = extendTheme({
+  fonts: {
+    heading: 'Raleway, sans-serif',
+    body: 'Open sans, sans-serif',
+  },
   colors: {
     gray: {
       100: "#F5f5f5",
